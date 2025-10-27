bdc84aeb06fe98950d6f26b80303a9422ed318d5
Ivannoe Tellez <204168705+ivantr033@users.noreply.github.com>
2025-10-27 20:12:36 +0300

    Initial commit

---
+++ orm.py
@@ -0,0 +1,21 @@
+import json
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from models import create_tables, Publisher, Shop, Book, Stock, Sale
+DSN = '...'
+engine = sqlalchemy.create_engine(DSN)
+create_tables(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+with open('fixtures/tests_data.json', 'r') as fd:
+    data = json.load(fd)
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
